4c87f9ed1b5a7b9ecd5091de35172dc187003b38
TheOptimust <73552453+Optimustprime@users.noreply.github.com>
2023-05-01 13:18:01 +0100

    Add files via upload

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -49,3 +49,20 @@ def deploy():
         return val
     except:
         return False
+
+
+def do_clean(number=0):
+    if number == 0 or number == 1:
+        with cd.local('./versions/'):
+            local("ls -lv | rev | cut -f 1 | rev | \
+            head -n +1 | xargs -d '\n' rm -rf")
+        with cd('/data/web_static/releases/'):
+            run("sudo ls -lv | rev | cut -f 1 | \
+            rev | head -n +1 | xargs -d '\n' rm -rf")
+    else:
+        with cd.local('./versions/'):
+            local("ls -lv | rev | cut -f 1 | rev | \
+            head -n +{} | xargs -d '\n' rm -rf".format(number))
+        with cd('/data/web_static/releases/'):
+            run("sudo ls -lv | rev | cut -f 1 | \
+            rev | head -n +{} | xargs -d '\n' rm -rf".format(number))
